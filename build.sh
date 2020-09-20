991baa20634ccc3cb79eebbf44b048951aee13fb
Mirko Velimirovic <bghryct@gmail.com>
2020-09-20 14:20:15 -0400

    continued adding claptrap to the presentation

--- build.sh
+++ build.sh
@@ -17,47 +17,25 @@ rm -rf master_ufo/ instance_ufo/
 
 echo "Build UFOS Removed"
 
-echo "vf cleaning"
+# We'll cover the below once we've done our first build. 
 
-vfs=$(ls ./source/vf/*.ttf)
-for vf in $vfs
-do
-	gftools fix-dsig -f $vf;
-	gftools fix-nonhinting $vf "$vf.fix";
-	mv "$vf.fix" $vf;
-	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
-	rtrip=$(basename -s .ttf $vf)
-	new_file=./source/vf/$rtrip.ttx;
-	rm $vf;
-	ttx $new_file
-	rm ./source/vf/*.ttx
-done
-rm ./source/vf/*backup*.ttf
+# echo "vf cleaning"
 
-echo "end vf cleaning"
-
-fontbakery check-googlefonts ./source/vf/*.ttf --ghmarkdown jost_problems.md
-# echo "Post processing"
-
-# ttfs=$(ls fonts/static/*.ttf)
-# echo $ttfs
-# for ttf in $ttfs
-# do
-# 	gftools fix-dsig -f $ttf;
-# 	gftools fix-nonhinting $ttf "$ttf.fix";
-# 	mv "$ttf.fix" $ttf;
-# done
-# echo "fixed nonhinting ttfs as well as DSIG"
-
-# rm ./fonts/static/*backup*.ttf
-
-
-
-# echo "fix vf meta? ok let's try that. I'm trying to fix VF meta"
-# gftools fix-vf-meta $vfs;
+# vfs=$(ls ./source/vf/*.ttf)
 # for vf in $vfs
 # do
+# 	gftools fix-dsig -f $vf;
+# 	gftools fix-nonhinting $vf "$vf.fix";
 # 	mv "$vf.fix" $vf;
+# 	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
+# 	rtrip=$(basename -s .ttf $vf)
+# 	new_file=./source/vf/$rtrip.ttx;
+# 	rm $vf;
+# 	ttx $new_file
+# 	rm ./source/vf/*.ttx
 # done
+# rm ./source/vf/*backup*.ttf
 
-# echo "Post processing complete"
+# echo "end vf cleaning"
+
+fontbakery check-googlefonts ./source/vf/*.ttf --ghmarkdown jost_problems.md
